852113231ea449c0bc528316decc3afeab2c6d6a
Omar Sanseviero <osanseviero@users.noreply.github.com>
2018-05-05 12:42:15 -0500

    feat: Change how admin sends info

--- app/src/main/java/com/example/osanseviero/proyectofinalmoviles/AdminRestaurantManagementFragment.java
+++ app/src/main/java/com/example/osanseviero/proyectofinalmoviles/AdminRestaurantManagementFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +17,29 @@ public class AdminRestaurantManagementFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_admin_restaurant_management, container, false);
-        Button createDishButton = rootView.findViewById(R.id.createDish);
 
+        // TODO: Create new table
+
+        // Navigate to dish creation view
+        Button createDishButton = rootView.findViewById(R.id.createDish);
         createDishButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("NAV", "Navigating to dish creation view");
                 AdminDishCreationFragment fragment = new AdminDishCreationFragment();
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction transaction = fm.beginTransaction();
+                transaction.replace(R.id.contentAdminFragment, fragment);
+                transaction.commit();
+            }
+        });
+
+        Button createMaterialButton = rootView.findViewById(R.id.adminCreateMaterial);
+        createMaterialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("NAV", "Navigating to material creation view");
+                AdminMaterialCreationFragment fragment = new AdminMaterialCreationFragment();
                 FragmentManager fm = getFragmentManager();
                 FragmentTransaction transaction = fm.beginTransaction();
                 transaction.replace(R.id.contentAdminFragment, fragment);
