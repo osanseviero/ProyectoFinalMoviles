af892026e6a2d1f0718d22240b2f5c97122f860d
Miguel <monterrubio.miguel@gmail.com>
2018-05-06 16:49:55 -0500

    feat: signup now has field validation (checks if empty) and automatically logs in created user

--- app/src/main/java/com/example/osanseviero/proyectofinalmoviles/SignUpActivity.java
+++ app/src/main/java/com/example/osanseviero/proyectofinalmoviles/SignUpActivity.java
@@ -3,9 +3,11 @@ package com.example.osanseviero.proyectofinalmoviles;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -34,8 +36,31 @@ public class SignUpActivity extends AppCompatActivity {
         password = findViewById(R.id.signupPassword);
     }
 
+    public void signupcheck(View v) {
+
+        if(TextUtils.isEmpty(username.getText())){
+            Toast.makeText(getApplicationContext(),"Campo de Usuario vacío",Toast.LENGTH_SHORT).show();
+        }
+
+        else if (TextUtils.isEmpty(name.getText())){
+            Toast.makeText(getApplicationContext(),"Campo de Nombre vacío",Toast.LENGTH_SHORT).show();
+        }
+
+        else if (TextUtils.isEmpty(email.getText())){
+            Toast.makeText(getApplicationContext(),"Campo de Email vacío",Toast.LENGTH_SHORT).show();
+        }
+
+        else if (TextUtils.isEmpty(password.getText())){
+            Toast.makeText(getApplicationContext(),"Campo de Contraseña vacío",Toast.LENGTH_SHORT).show();
+        }
+
+        else{
+            createUser(v);
+        }
+    }
+
     public void createUser(View v) {
-        final Intent intent = new Intent(this, LoginActivity.class);
+        final Intent intent = new Intent(this, ClientHomeScreenActivity.class);
 
         JSONObject js = new JSONObject();
         try {
@@ -47,7 +72,7 @@ public class SignUpActivity extends AppCompatActivity {
             js.put("email", email.getText().toString());
             js.put("password", password.getText().toString());
 
-            // This is for customers and no token is needed for this oepration
+            // This is for customers and no token is needed for this operation
             js.put("kind", 5);
             js.put("token", "");
             Log.d("DBG", js.toString());
@@ -58,7 +83,7 @@ public class SignUpActivity extends AppCompatActivity {
 
         //TODO: Have a loading bar or something like that
 
-        // Create JSON request
+        // Create JSON request for user creation
         String url = "http://docker-azure.cloudapp.net/user/create";
         JsonObjectRequest jsonObjReq = new JsonObjectRequest(
                 Request.Method.POST, url, js,
@@ -72,7 +97,7 @@ public class SignUpActivity extends AppCompatActivity {
                             Log.d("DBG", "User id: " + id);
 
                             //TODO: Show a success message
-                            startActivity(intent);
+                            Toast.makeText(getApplicationContext(), "Usuario creado exitosamente", Toast.LENGTH_SHORT).show();
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -83,11 +108,65 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onErrorResponse(VolleyError error) {
                 //TODO: Handle error
+
+                Toast.makeText(getApplicationContext(), new String(error.networkResponse.data), Toast.LENGTH_SHORT).show();
                 Log.e("ERROR", "Error code: " + error.networkResponse.statusCode);
                 Log.e("err", "Message:" + new String(error.networkResponse.data));
             }
         });
 
         Volley.newRequestQueue(this).add(jsonObjReq);
+
+        //JSON request for login
+        JSONObject js2 = new JSONObject();
+        try {
+            // Build JSON from text fields
+            js2.put("username", username.getText().toString());
+            js2.put("password", password.getText().toString());
+            Log.d("DBG", js2.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        String url2 = "http://docker-azure.cloudapp.net/user/login";
+        JsonObjectRequest jsonObjReq2 = new JsonObjectRequest(
+                Request.Method.POST, url2, js2,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String token = response.getString("token");
+                            String kind = response.getString("kind");
+
+                            Log.d("DBG", "token: " + token);
+                            Log.d("DBG", "kind: " + kind);
+
+                            Toast.makeText(getApplicationContext(), "Login Exitoso", Toast.LENGTH_SHORT).show();
+
+                            //TODO: Check kind and send to corresponding screen
+
+                            intent.putExtra("token", token);
+                            Log.i("NAV", "Abriendo home screen del cliente.");
+                            startActivity(intent);
+                        }
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //TODO: Handle error
+
+                Toast.makeText(getApplicationContext(), new String(error.networkResponse.data), Toast.LENGTH_SHORT).show();
+                Log.e("ERROR", "Error code: " + error.networkResponse.statusCode);
+                Log.e("err", "Message:" + new String(error.networkResponse.data));
+            }
+        });
+
+
+        Volley.newRequestQueue(this).add(jsonObjReq2);
     }
 }
