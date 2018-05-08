858d2e6e515de2bb6909f8b8eab4b75412baa90d
Omar Sanseviero <osanseviero@users.noreply.github.com>
2018-05-08 16:22:02 -0500

    feat: Add view with tables and allow to assign users

---
+++ app/src/main/java/com/example/osanseviero/proyectofinalmoviles/WaiterAsssignUserFragment.java
@@ -0,0 +1,113 @@
+package com.example.osanseviero.proyectofinalmoviles;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class WaiterAsssignUserFragment extends Fragment {
+    Integer table;
+    TextView tableTextview;
+    EditText tableCustomer;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_waiter_assign_user, container, false);
+        tableCustomer =  rootView.findViewById(R.id.tableAssignUser);
+
+        // Extract table
+        table = 0;
+        Bundle bundle = this.getArguments();
+        if(bundle != null) {
+            table = bundle.getInt("table");
+            tableTextview = rootView.findViewById(R.id.tableTitle);
+            tableTextview.setText("MESA " + table);
+            Button b = rootView.findViewById(R.id.assignTable);
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    JSONObject js = new JSONObject();
+                    try {
+                        js.put("token", ((WaiterHomeScreenActivity) getContext()).token );
+                        js.put("table", table);
+
+                        ArrayList<String> customers = new ArrayList<String>();
+                        customers.add(tableCustomer.getText().toString());
+
+                        js.put("customers", customers);
+                        Log.d("DBG", js.toString());
+
+                        String url = "http://docker-azure.cloudapp.net/tab/create";
+                        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                                Request.Method.POST, url, js,
+                                new Response.Listener<JSONObject>() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        try {
+                                            String message = response.getString("message");
+                                            String id = response.getString("id");
+
+                                            Log.d("DBG", "Message: " + message);
+                                            Log.d("DBG", "Id: " + id);
+
+                                            Toast.makeText(getContext(), "Mesa asignada correctamente", Toast.LENGTH_SHORT).show();
+
+                                            WaiterTablesFragment waiterTablesFragment = new WaiterTablesFragment();
+                                            FragmentManager wtfm = getFragmentManager();
+                                            FragmentTransaction reportFragmentTransaction = wtfm.beginTransaction();
+                                            reportFragmentTransaction.replace(R.id.contentWaiterFragment, waiterTablesFragment);
+                                            reportFragmentTransaction.commit();
+
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+
+                                    }
+                                },
+                                new Response.ErrorListener() {
+
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        Toast.makeText(getContext(),  new String(error.networkResponse.data), Toast.LENGTH_SHORT).show();
+
+                                        Log.e("ERROR", "Error code: " + error.networkResponse.statusCode);
+                                        Log.e("err", "Message:" + new String(error.networkResponse.data));
+                                    }
+                                });
+
+                        Volley.newRequestQueue(getContext()).add(jsonObjReq);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+
+
+        return rootView;
+    }
+}
--- app/src/main/java/com/example/osanseviero/proyectofinalmoviles/WaiterTablesFragment.java
+++ app/src/main/java/com/example/osanseviero/proyectofinalmoviles/WaiterTablesFragment.java
@@ -1,20 +1,151 @@
 package com.example.osanseviero.proyectofinalmoviles;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 
 public class WaiterTablesFragment extends Fragment {
+    Integer[] available;
+    String tables;
+    Integer counter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_waiter_tables, container, false);
+
+        final JSONObject js = new JSONObject();
+        try {
+            js.put("token", ((WaiterHomeScreenActivity) getContext()).token );
+            Log.d("DBG", js.toString());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        String url = "http://docker-azure.cloudapp.net/tables/";
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                Request.Method.POST, url, js,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String url = "http://docker-azure.cloudapp.net/tables/available";
+                        Log.d("DBG", url);
+                        try {
+                            tables = response.getString("tables");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        JsonObjectRequest req = new JsonObjectRequest(
+                                Request.Method.POST, url, js,
+                                new Response.Listener<JSONObject>() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        try {
+                                            Log.d("DBG", "Getting available tables");
+                                            available = getIntegerArray("tables", response);
+
+
+                                            LinearLayout linearLayout = getActivity().findViewById(R.id.tableList);
+                                            Log.d("DBG", "# of available tables: " + available);
+                                            for(counter=1; counter<=Integer.parseInt(tables); counter++) {
+                                                Button b = new Button(getActivity());
+                                                b.setTag(counter);
+                                                b.setText("Mesa " + Integer.toString(counter));
+
+                                                if(Arrays.asList(available).contains(counter)) {
+                                                    b.setBackgroundColor(Color.GREEN);
+
+                                                    b.setOnClickListener(new View.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(View v) {
+                                                            WaiterAsssignUserFragment waiterAsssignUserFragment = new WaiterAsssignUserFragment();
+                                                            FragmentManager waufm = getFragmentManager();
+                                                            FragmentTransaction reportFragmentTransaction = waufm.beginTransaction();
+                                                            reportFragmentTransaction.replace(R.id.contentWaiterFragment, waiterAsssignUserFragment);
+
+                                                            // Specify category name
+                                                            Bundle args = new Bundle();
+                                                            args.putInt("table", Integer.parseInt(v.getTag().toString()));
+                                                            waiterAsssignUserFragment.setArguments(args);
+                                                            reportFragmentTransaction.commit();
+                                                        }
+                                                    });
+                                                } else {
+                                                    b.setBackgroundColor(Color.RED);
+                                                }
+                                                linearLayout.addView(b);
+                                            }
+
+
+
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+
+                                    }
+                                }, new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                //TODO: Handle error
+                                Log.e("ERROR", "Error code: " + error.networkResponse.statusCode);
+                                Log.e("err", "Message:" + new String(error.networkResponse.data));
+                            }
+                        });
+
+                        Volley.newRequestQueue(rootView.getContext()).add(req);
+
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //TODO: Handle error
+                Log.e("ERROR", "Error code: " + error.networkResponse.statusCode);
+                Log.e("err", "Message:" + new String(error.networkResponse.data));
+            }
+        });
+
+        Volley.newRequestQueue(rootView.getContext()).add(jsonObjReq);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_waiter_tables, container, false);
+        return rootView;
+    }
+
+    Integer[] getIntegerArray(String name, JSONObject object) throws JSONException {
+        JSONArray jsonArray = object.getJSONArray(name);
+        Integer[] array = new Integer[jsonArray.length()];
+        for (int i = 0; i < jsonArray.length(); i++){
+            array[i] = Integer.parseInt(jsonArray.getString(i));
+        }
+        return array;
     }
 
 }
