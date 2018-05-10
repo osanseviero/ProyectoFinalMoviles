01defe5ef1e968d8a92806b739e5f262793fbbdf
franspaco <franspaco@gmail.com>
2018-05-10 01:42:35 -0500

    feat: client graph

--- app/src/main/java/com/example/osanseviero/proyectofinalmoviles/clientFragments/ClientOrderListFragment.java
+++ app/src/main/java/com/example/osanseviero/proyectofinalmoviles/clientFragments/ClientOrderListFragment.java
@@ -6,13 +6,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.example.osanseviero.proyectofinalmoviles.R;
+import com.example.osanseviero.proyectofinalmoviles.graphs.Graph;
+import com.github.mikephil.charting.charts.PieChart;
 
 public class ClientOrderListFragment extends Fragment {
+    View rootview;
+
+    PieChart pieChart;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order_list, container, false);
+        rootview = inflater.inflate(R.layout.fragment_order_list, container, false);
+
+        pieChart = rootview.findViewById(R.id.pieChart_client);
+
+        Graph.loadPieData(pieChart, "customer/tabs", "Categorias", rootview.getContext());
+
+        return rootview;
     }
 }
